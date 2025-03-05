57ece01e7c888afa52f7e9165222420545a25d17
Dmitry Mikushin <dmitry@kernelgen.org>
2025-03-05 23:01:07 +0100

    Getting containers to start properly, still need to fix the network

---
+++ env_file.sh
@@ -0,0 +1,11 @@
+export USER_UID=1000
+export USER_GID=1000
+export FORGEJO__database__DB_TYPE=mysql
+export FORGEJO__database__HOST=db:3306
+export FORGEJO__database__NAME=forgejo
+export FORGEJO__database__USER=forgejo
+export FORGEJO__database__PASSWD=forgejo
+export MYSQL_ROOT_PASSWORD=forgejo
+export MYSQL_USER=forgejo
+export MYSQL_PASSWORD=forgejo
+export MYSQL_DATABASE=forgejo
